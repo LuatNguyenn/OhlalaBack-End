dcf9161f7defa57e186d2851bba5fdd1d1d03d0a
LuatNguyenn <luatnguyenn61@gmail.com>
2022-06-19 21:03:25 -0600

    up 2

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const bodyParser = require("body-parser");
 const nodemailer = require("nodemailer");
 const path = require("path");
